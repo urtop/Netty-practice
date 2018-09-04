7273e64dec298f9260afbba676343999712bf82a
Mark <urtop@qq.com>
2018-09-04 21:53:27 +0800

    add first demo

---
+++ src/main/java/basicDemo/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package basicDemo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        ByteBuf in = (ByteBuf) msg;
+//        while (in.isReadable()) {
+//            System.out.print((char) in.readByte());
+//            System.out.flush();
+//        }
+//        System.out.println();
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
